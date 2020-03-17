34070031f4e17dc06023c2a88faa07466bd7c69c
Crayonjk <jikaigg@outlook.com>
2020-03-17 23:30:36 +0800

    20200317222

---
+++ SpringMVC/SpringMVC_kuang/01_springmvc_start/src/main/java/com/jikaigg/controller/HelloController.java
@@ -0,0 +1,53 @@
+package com.jikaigg.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+    @RequestMapping("hello")
+    public String handleRequest(Model model){
+        model.addAttribute("msg","helloController!!!");
+
+        return "hello";
+    }
+
+
+    @GetMapping("add/{a}/{b}")
+    public String add1(@PathVariable int a, @PathVariable int b, Model model){
+        int res = a+b;
+        model.addAttribute("msg","helloController1!!!"+res);
+        return "hello";
+    }
+    @PostMapping("add/{a}/{b}")
+    public String add2(@PathVariable int a, @PathVariable int b, Model model){
+        int res = a+b;
+        model.addAttribute("msg","helloController2!!!"+res);
+        return "hello";
+    }
+    @DeleteMapping("add/{a}/{b}")
+    public String add3(@PathVariable int a, @PathVariable int b, Model model){
+        int res = a+b;
+        model.addAttribute("msg","helloController3!!!"+res);
+        return "hello";
+    }
+    @PutMapping("add/{a}/{b}")
+    public String add4(@PathVariable int a, @PathVariable int b, Model model){
+        int res = a+b;
+        model.addAttribute("msg","helloController4!!!"+res);
+        return "hello";
+    }
+
+
+    @RequestMapping("zhaungfa")
+    public String test2(){
+        return "/index.jsp";
+    }
+
+    @RequestMapping("redirect")
+    public String test3(){
+        return "redirect:/index.jsp";
+    }
+}
---
+++ SpringMVC/SpringMVC_kuang/01_springmvc_start/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>index</title>
+</head>
+<body>
+<form action="/add/" method="post">
+    <input type="text" name="a">
+    <input type="text" name="b">
+    <input type="submit">
+
+</form>
+</body>
+</html>

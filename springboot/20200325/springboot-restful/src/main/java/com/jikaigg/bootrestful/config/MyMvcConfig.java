78743a7b5975b46f196eabfedf1f8d084ddbd11d
Crayonjk <jikaigg@outlook.com>
2020-03-27 00:07:02 +0800

    20200327222

--- springboot/20200325/springboot-restful/src/main/java/com/jikaigg/bootrestful/config/MyMvcConfig.java
+++ springboot/20200325/springboot-restful/src/main/java/com/jikaigg/bootrestful/config/MyMvcConfig.java
@@ -1,9 +1,15 @@
 package com.jikaigg.bootrestful.config;
 
+import com.jikaigg.bootrestful.component.LoginHandlerInterceptor;
+import com.jikaigg.bootrestful.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.standard.expression.GreaterOrEqualToExpression;
 
 @Configuration
 public class MyMvcConfig extends WebMvcConfigurerAdapter{
@@ -17,12 +23,34 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter{
     @Bean
     public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
         WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName("index");
-                registry.addViewController("/index.html").setViewName("index");
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/login.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+
+            }
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**").addResourceLocations("classpath:/staitc/");
+                super.addResourceHandlers(registry);
+            }
+
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/login.html","/","/user/login");
+
             }
         };
         return adapter;
     }
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+
 }

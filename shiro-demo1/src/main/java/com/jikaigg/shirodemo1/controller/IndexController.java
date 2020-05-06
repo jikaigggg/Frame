99c46f24026658822e14a2894a7902e2b09e6c0c
jikaigggg <jikaigg@gmail.com>
2020-05-06 19:16:17 +0800

    20200506111

--- shiro-demo1/src/main/java/com/jikaigg/shirodemo1/controller/IndexController.java
+++ shiro-demo1/src/main/java/com/jikaigg/shirodemo1/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.jikaigg.shirodemo1.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
@@ -15,15 +17,18 @@ import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.RespectBinding;
 
 @Controller
+@Slf4j
 public class IndexController {
     @RequestMapping(value = {"/","index"})
     public String toIndex(Model model){
+        log.info("hello");
         model.addAttribute("msg","yaojikai");
         return "index";
     }
 
     @RequestMapping("/user/add")
     public String add(){
+        log.info("go to login");
         return "user/add";
     }
 
@@ -46,14 +51,18 @@ public class IndexController {
         //执行登陆的方法
         try {
             subject.login(token);
+            log.info("try username id {} , password is {} ",token.getUsername(),token.getPassword());
             return "index";
         } catch (UnknownAccountException e) {
+            log.info("one username id {} , password is {} ",token.getUsername(),token.getPassword());
             model.addAttribute("msg","用户名不存在");
             return "login";
         } catch (IncorrectCredentialsException e) {
+            log.info("two username id {} , password is {} ",token.getUsername(),token.getPassword());
             model.addAttribute("msg","密码错误");
             return "login";
         } catch (AuthenticationException e) {
+            log.info("three username id {} , password is {} ",token.getUsername(),token.getPassword());
             model.addAttribute("msg","其他问题");
             return "login";
         }
